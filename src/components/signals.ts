5c31d7517f3c6e6a9b43bd90df3261f35850d36a
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2024-06-09 03:44:40 +0430

    Toggle signals

--- src/components/signals.ts
+++ src/components/signals.ts
@@ -19,28 +19,35 @@ import { guessable }                    from "../pkg/rust_lib";
 
 
 const [password, setPassword] = createSignal(generate_password(16, true, true, true));
-const [length, setLength] = createSignal(16);
 
-createEffect(() => {
-	document.getElementById("gp")!.textContent = generate_password(
-		length(),
-		true,
-		true,
-		true,
-	).toString();
+const [length, setLength]                               = createSignal(16);
+const [addSpecialCha, setAddSpecialCha]                 = createSignal(true);
+const [addNumber, setAddNumber]                         = createSignal(true);
+const [capitalizeFirstLetter, setCapitalizeFirstLetter] = createSignal(true);
+
+createEffect(() => { setPassword(generate_password(
+      length(),
+      addSpecialCha(),
+      addNumber(),
+      capitalizeFirstLetter(),
+   ));
 });
 
 
+createEffect(() => { document.getElementById("gp")!
+   .textContent = password() });
+
+createEffect(() => { document.getElementById("gu")!
+   .textContent = guessable(password()) });
+
+createEffect(() => { document.getElementById("s1")!
+   .textContent = calculate_password_strength(password()) });
+
+createEffect(() => { document.getElementById("s2")!
+   .textContent = calculate_password_strength2(password()) });
 
 
-const guess = guessable(password);
-const strength = calculate_password_strength(password);
-const strength2 = calculate_password_strength2(password);
 
-
-document.getElementById("gu")!.textContent = guess.toString();
-document.getElementById("s1")!.textContent = strength.toString();
-document.getElementById("s2")!.textContent = strength2.toString();
 
 
 
@@ -49,18 +56,28 @@ document.getElementById("maina")!.addEventListener("input",(e)=>{
       const value = (e!.target as HTMLInputElement).value;
       setLength(Number(value));      
    }
+});
 
-   if((e!.target as HTMLInputElement).matches("#Input")){
 
-      const value = (e!.target as HTMLInputElement).value;
-      const guess = guessable(value);
-      const strength = calculate_password_strength(value);
-      const strength2 = calculate_password_strength2(value);
 
-      document.getElementById("gu")!.textContent = guess.toString();
-      document.getElementById("s1")!.textContent = strength.toString();
-      document.getElementById("s2")!.textContent = strength2.toString();
-      
+document.getElementById("maina")!.addEventListener("change",(e)=>{
+   if((e!.target as HTMLInputElement).matches("#add_special_cha")){
+      if ((e!.target as HTMLInputElement).checked){ 
+         setAddSpecialCha(true) } else { setAddSpecialCha(false) 
+      }
    }
+
+   if((e!.target as HTMLInputElement).matches("#add_number")){
+      if ((e!.target as HTMLInputElement).checked){ 
+         setAddNumber(true) } else { setAddNumber(false) 
+      }
+   }
+
+   if((e!.target as HTMLInputElement).matches("#capitalize_first_letter")){
+      if ((e!.target as HTMLInputElement).checked){ 
+         setCapitalizeFirstLetter(true) } else { setCapitalizeFirstLetter(false) 
+      }
+   }
+
 });
       
