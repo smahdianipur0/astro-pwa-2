444131cdb0ff8c480ad0495b1afb2882049a2ce8
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2024-06-09 23:27:00 +0430

    Generate Card

--- src/components/signals.ts
+++ src/components/signals.ts
@@ -16,8 +16,6 @@ import { calculate_password_strength2 } from "../pkg/rust_lib";
 import { guessable }                    from "../pkg/rust_lib";
 
 
-
-
 const [password, setPassword] = createSignal(generate_password(16, true, true, true));
 
 const [length, setLength]                               = createSignal(16);
@@ -45,6 +43,31 @@ createEffect(() => { document.getElementById("s1")!
 
 createEffect(() => { document.getElementById("s2")!
    .textContent = calculate_password_strength2(password()) });
+
+
+createEffect(() => { 
+   const gu= guessable(password());
+   if (gu === "Too Guessable") {
+      document.getElementById("box")!.style.setProperty("--box-w", "36%");
+      document.getElementById("box")!.style.setProperty("--box-p", "0%");
+   }   if (gu === "Very Guessable") {
+      document.getElementById("box")!.style.setProperty("--box-w", "52%");
+      document.getElementById("box")!.style.setProperty("--box-p", "16%");
+   }   if (gu === "Somewhat Guessable") {
+      document.getElementById("box")!.style.setProperty("--box-w", "68%");
+      document.getElementById("box")!.style.setProperty("--box-p", "32%");
+   }   if (gu === "Safely Unguessable") {
+      document.getElementById("box")!.style.setProperty("--box-w", "84%");
+      document.getElementById("box")!.style.setProperty("--box-p", "48%");
+   }   if (gu === "Very Unguessable") {
+      document.getElementById("box")!.style.setProperty("--box-w", "100%");
+      document.getElementById("box")!.style.setProperty("--box-p", "64%");
+   }
+});
+
+
+
+
 
 
 
@@ -80,4 +103,21 @@ document.getElementById("maina")!.addEventListener("change",(e)=>{
    }
 
 });
-      
+
+document.getElementById("maina")!.addEventListener("click",(e)=>{
+   if((e!.target as HTMLInputElement).matches("#gp ,#ttc")){
+      navigator.clipboard.writeText(password());   
+
+   }
+   if((e!.target as HTMLInputElement).matches("#redo")){
+      setPassword(generate_password(
+      length(),
+      addSpecialCha(),
+      addNumber(),
+      capitalizeFirstLetter(),
+   ));
+
+   }
+});
+
+
