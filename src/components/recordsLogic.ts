857a378e23ab1c46cfa133e620d1ff67b08fcc8e
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2025-03-26 04:44:53 +0430

    status and role.

--- src/components/recordsLogic.ts
+++ src/components/recordsLogic.ts
@@ -3,15 +3,37 @@ import { createSignal, createEffect } from "solid-js";
 import {dbCreate, dbDelete, dbReadAll, type ReadAllResultTypes} from "../utils/surrealdb-indexed"
 
 const [createVaultName, setCreateVaultName] = createSignal("");
-const [vaults, setVaults] = createSignal<ReadAllResultTypes["Vaults"]>([]);
+const [vaultsList, setVaultsList] = createSignal<ReadAllResultTypes["Vaults"]>([]);
+const [creationAllowed, setCreationAllowed] = createSignal(true);
+
+
 
 const intialvaults = await dbReadAll("Vaults");
-if (intialvaults){setVaults(intialvaults);};
+if (intialvaults){setVaultsList(intialvaults);};
+
+createEffect(() => { 
+    const filteredItems = vaultsList().filter(item => item.status === "available" && item.role === "owner");
+    setCreationAllowed(filteredItems.length < 3);
+});
+
+createEffect(() => { 
+    (async () => {
+        const vaultHint = (await element.wait("#vault-hint")) as HTMLElement;
+        if (creationAllowed()){
+            vaultHint.textContent = "You can create a new vault. You can have up to 3 vaults in total" 
+        } else {
+            vaultHint.textContent = "You already own 3 vaults, which is the maximum allowed. Need a new one? Try deleting an existing vault first."
+        }
+    })();
+});
+
+
 
 (async () => {
 
     const vaultMenu = (await element.wait("#vault-menu")) as HTMLElement;
     const createVaultButton = ( await element.wait("#create-vault-button")) as HTMLButtonElement
+    const vaultNameInput = ( await element.wait("#vault-name-input")) as HTMLInputElement
 
     vaultMenu.addEventListener("click",(e)=>{
         if((e!.target as HTMLInputElement).matches("#vault-create")){
@@ -30,15 +52,21 @@ if (intialvaults){setVaults(intialvaults);};
 
             (async () => {
                 console.log("created");
-                await dbCreate("Vaults:create", {name:createVaultName(), crreatedAt: new Date().toISOString() });
+                await dbCreate("Vaults:create", {
+                    name:createVaultName(), 
+                    crreatedAt: new Date().toISOString(),
+                    status: "available",
+                    role: "owner" 
+                });
+
                 const vaults = await dbReadAll("Vaults");
-                if (vaults){setVaults(vaults)};
+                if (vaults){setVaultsList(vaults)};
             })();
         };
     });
 
-    createEffect(() => { createVaultButton.disabled = (createVaultName() === "") ;});
-
+    createEffect(() => { createVaultButton.disabled = ( !creationAllowed() ||createVaultName() === "") ;});
+    createEffect(() => { vaultNameInput.disabled = (!creationAllowed()) });
 })();
 
 createEffect(() => { 
@@ -46,9 +74,9 @@ createEffect(() => {
     vaultlist.textContent = "";
     vaultlist.disabled = false;
     const fragment = document.createDocumentFragment();
-    if (vaults().length === 0){
+    if (vaultsList().length === 0){
         fragment.append(element.configure("option", {textContent: "No vaults found"}));
-    } else { (vaults() ?? [])
+    } else { (vaultsList() ?? [])
         .sort((a, b) => new Date(b.crreatedAt).getTime() - new Date(a.crreatedAt).getTime())
         .forEach((entry) => {
             fragment.append( element.configure("option", {textContent:entry.name}));
--- src/utils/surrealdb-indexed.ts
+++ src/utils/surrealdb-indexed.ts
@@ -25,7 +25,7 @@ export type Schemas = {
 	Emails: { email: string; crreatedAt: string };
 	RecentDelPass: {  title: string; password: string; crreatedAt: string };
 	Credentials: { registered:boolean; UID: string;};
-	Vaults: {name: string; crreatedAt: string}
+	Vaults: {name: string; crreatedAt: string, status:"available" | "deleted", role:"owner" | "viewer"}
 };
 
 
