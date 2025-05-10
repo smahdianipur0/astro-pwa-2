0034b9fe888bd7ecdc2462b1c735c0b64c83b6b3
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2025-05-11 02:07:34 +0430

    fixed cards array styling.

--- src/components/cardsArrayLogic.ts
+++ src/components/cardsArrayLogic.ts
@@ -21,48 +21,51 @@ createEffect(() => {(async () => {
     .forEach((entry) => {
 
       fragment.append(
-        element.configure('div', { className: 'entry-item', append: [
-          element.configure('div', {append: [
+      	element.configure('div', {className:"card-border", style:"margin-block: var(--gap-x04);", append:[
+        	element.configure('div',  { className: "card glass box-shadow", style:"padding: var(--gap-x04);", append: [
+         		element.configure('div', { className: 'flex-spread-childs',append: [
 
-            element.configure('p', { className: 'hint ellipsis', 
-              style: "width: 20ch",
-              textContent: entry.name || 'untitled' 
-          	}),
+		            element.configure('p', { className: 'ellipsis', 
+		              style: "width: 20ch; font-weight: 600; font-style: italic;",
+		              textContent: entry.name || 'untitled' 
+		          	}),
 
-			element.configure('div', { append: 
-				(entry.data ?? []).map((dataItem) => {
-			    return element.configure('button', { dataset: {action: 'copy'},
-			    	textContent: dataItem,
-			    	className: 'ellipsis',
-			    	style:"text-align: start; width: 19ch;",
-			    });
-			  })
-			}),
+	            	element.configure('div', {className: 's-container',append: [
 
-          ]}),
+			            element.configure('details', { 
+			              name: 'delete-item', 
+			              className: 'right-to-left',
+			              append: [
+			                element.configure('summary',{ className: 'right-to-left' }), 
+			                element.configure('button', { dataset: {action: 'delete'},
+			                  id: entry.id?.id ?? '',
+			                  textContent: ' Delete'}),
+			              ]
+			            }),
 
-          element.configure('div', {className: 's-container',append: [
+			            element.configure('button', { dataset: {action: 'update'},
+			            	id: entry.id?.id ?? '', 
+			            	append: [
+			              element.draw("svg", { style: "width: var(--gap-x04); height: var(--gap-x04);",viewBox: "0 0 24 24", fill: "none",append: 
+			                element.draw("path", {d: "M21.03 2.97a3.578 3.578 0 0 1 0 5.06L9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L15.97 2.97a3.578 3.578 0 0 1 5.06 0ZM15 6.06 5.062 16a.75.75 0 0 0-.193.333l-1.05 3.85 3.85-1.05A.75.75 0 0 0 8 18.938L17.94 9 15 6.06Zm2.03-2.03-.97.97L19 7.94l.97-.97a2.079 2.079 0 0 0-2.94-2.94Z"})
+			              })
+			            ]}),
 
-            element.configure('details', { 
-              name: 'delete-item', 
-              className: 'right-to-left',
-              append: [
-                element.configure('summary',{ className: 'right-to-left' }), 
-                element.configure('button', { dataset: {action: 'delete'},
-                  id: entry.id?.id ?? '',
-                  textContent: ' Delete'}),
-              ]
-            }),
+		          	]})
 
-            element.configure('button', { dataset: {action: 'update'},
-            	id: entry.id?.id ?? '', 
-            	append: [
-              element.draw("svg", { style: "width: var(--gap-x04); height: var(--gap-x04);",viewBox: "0 0 24 24", fill: "none",append: 
-                element.draw("path", {d: "M21.03 2.97a3.578 3.578 0 0 1 0 5.06L9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L15.97 2.97a3.578 3.578 0 0 1 5.06 0ZM15 6.06 5.062 16a.75.75 0 0 0-.193.333l-1.05 3.85 3.85-1.05A.75.75 0 0 0 8 18.938L17.94 9 15 6.06Zm2.03-2.03-.97.97L19 7.94l.97-.97a2.079 2.079 0 0 0-2.94-2.94Z"})
-              })
-            ]}),
-          ]})
-        ]})
+          		]}),
+
+	          	element.configure('div', { style:"display: flex; flex-direction: column;", append: 
+					(entry.data ?? []).map((dataItem) => {
+				    return element.configure('button', { dataset: {action: 'copy'},
+				    	textContent: dataItem,
+				    	className: 'ellipsis',
+				    	style:"text-align: start; width: 19ch; font-weight: 200",
+				    });
+				  })
+				}),
+        	]}),
+        ]}),
       );
     });
 
