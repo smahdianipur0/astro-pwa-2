9114692cf51ea382657a7300598cb1054e54a3b9
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2025-05-17 01:14:36 +0430

    toast animation

--- src/components/homeLogic.ts
+++ src/components/homeLogic.ts
@@ -318,8 +318,13 @@ export function showToast() {
   const toastElement = document.getElementById('toast')!;
   if (toastElement) {
     toastElement.style.bottom = 'var(--gap-x10)';
+    toastElement.style.scale = '1';
+    toastElement.style.transition = 'bottom 0.25s var(--move), scale 0.5s var(--appear)';
+
     timeoutId = window.setTimeout(() => {
       toastElement.style.bottom = '-100%';
+      toastElement.style.scale = '0.55';
+      toastElement.style.transition = 'bottom 0.4s ease-in, scale 0.4s ease-in';
     }, 2000);
   }
 }
