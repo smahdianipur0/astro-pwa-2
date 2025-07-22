92227def91422de98584bae3d5a1b02dfc227a1f
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2025-07-22 14:04:30 +0430

    updated element utils

--- src/utils/elementUtils.ts
+++ src/utils/elementUtils.ts
@@ -1,56 +1,5 @@
-type ElementProps = {
-  className?: string;
-  id?: string;
-  textContent?: string;
-  dataset?: {[key: string]: string;};
-  append?: (HTMLElement | SVGElement) | (HTMLElement | SVGElement)[];
-  [key: string]: any;
-};
-
-type ValidTagNames = keyof HTMLElementTagNameMap;
-type ValidSvgTagNames = keyof SVGElementTagNameMap;
-
 export const el = {
-    c: <T extends ValidTagNames>(tag: T, { append, dataset, ...props }: ElementProps): HTMLElementTagNameMap[T] => {
-    const element = document.createElement(tag) as HTMLElementTagNameMap[T];
     
-    if (append) {
-      (Array.isArray(append) ? append : [append]).forEach(child =>
-        element.appendChild(child)
-      );
-    }
-
-    if (dataset) {
-      Object.assign(element.dataset, dataset);
-    }
-
-    return Object.assign(element, props);
-  },
-
-  d: <T extends ValidSvgTagNames>(tag: T,{ append, dataset, style, ...props }: ElementProps): SVGElementTagNameMap[T] => {
-    const element = document.createElementNS("http://www.w3.org/2000/svg",tag) as SVGElementTagNameMap[T];
-  
-    if (append) {
-      (Array.isArray(append) ? append : [append]).forEach(child =>
-        element.appendChild(child)
-      );
-    }
-  
-    if (dataset) {
-      Object.assign(element.dataset, dataset);
-    }
-  
-    if (typeof style === "string") {element.style.cssText = style;}
-  
-    Object.entries(props).forEach(([key, value]) => {
-      if (value != null) {
-        element.setAttribute(key, value.toString());
-      }
-    });
-  
-    return element;
-  },
-
   wait: (selector: string): Promise<HTMLElement> => {
     return new Promise(resolve => {
       const element = document.querySelector(selector);
