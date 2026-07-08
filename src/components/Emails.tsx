9c13143fbc159f3923828f47ede12d2f1e2f6582
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2026-07-08 08:25:20 +0430

    dispose?.(), emails.tsx, drawer styling

--- src/components/Emails.tsx
+++ src/components/Emails.tsx
@@ -1,61 +1,67 @@
-import type { JSX } from "solid-js";
-import type { ReadResultTypes } from "../utils/surrealdb-indexed"
+import type { ReadAllResultTypes } from "../utils/surrealdb-indexed"
 import Popover from "./ui/Popover";
 import DeleteMenu from './ui/DeleteMenu'
+import { showToast } from "../logic/misc";
+import { email } from "../logic/email";
 
 
-export function Emails(entry:ReadResultTypes["Emails"], isEditing:boolean): JSX.Element {
+const EmailsList = (entries:ReadAllResultTypes["Emails"], isEditing:boolean) => {
 
     return(<>
+        {entries.length !== 0 && ( entries.map((entry) => (
+            <li  
+                data-swapy-slot={entry.createdAt}
+                style='width: 100%; 
+                background-color:transparent; 
+                padding-inline: 0;'>  
 
-        <li  
-            data-swapy-slot={entry.createdAt}
-            style='width: 100%; 
-            background-color:transparent; 
-            padding-inline: 0;'>  
+                <div data-swapy-item={entry.id} class="HStack" style="height: var(--size-sm4); width:var(--stretch);">
 
-            <div data-swapy-item={entry.id} class="HStack" style="height: var(--size-sm4); width:var(--stretch);">
+                    <div>
+                        <button 
+                            class="not-prose ellipsis" 
+                            onClick={() => { 
+                                navigator.clipboard.writeText(entry.email ?? "");
+                                showToast();
+                            }}
+                            style={`text-align: start; 
+                            width: ${isEditing ? " var(--size-xl2) ;" : "calc(var(--size-xl4) - var(--size-sm2));"};
+                            font-weight: 700;
+                            color: oklch(var(--gray-95));
+                            padding: var(--padding-0);
+                            padding-inline: 0;`}>
+                            {entry.email ?? ''} </button>
+                    </div>
 
-                <div>
-                    <button 
-                        id = {entry.email ?? ''}
-                        data-action='copy' 
-                        class="not-prose ellipsis" 
-                        style={`text-align: start; 
-                        width: ${isEditing ? " var(--size-xl2) ;" : "calc(var(--size-xl4) - var(--size-sm2));"};
-                        font-weight: 700;
-                        color: oklch(var(--gray-95));
-                        padding: var(--padding-0);
-                        padding-inline: 0;`}>
-                        {entry.email ?? ''} </button>
+                    {isEditing && <div class="HStack slide-in-right">
+
+                        <div class="HStack">
+                            <Popover 
+                                trigger={
+                                    <div class='text-as-button ZStack' style="margin-right: calc(var(--size-sm0)* -1);" >
+                                        <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM18 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" fill="oklch(var(--gray-95))"/></svg>
+                                    </div>
+                                }
+                            >
+                                <menu class="glass subtle-shadow menu-horizontal">
+                                    <li>
+                                        <DeleteMenu  content= {
+                                            <button   onClick={() => entry.id && email.deleteEmail(entry.id)}>Delete</button>  
+                                        }></DeleteMenu>                                
+                                    </li>
+                                </menu>
+                            </Popover>                  
+                        </div>
+
+                        <div data-swapy-handle style="width: var(--size-md2);display: flex;justify-content: flex-end;" > 
+                            <div class="swapy-handle"></div> 
+                        </div>
+
+                    </div> }
                 </div>
-
-                {isEditing && <div class="HStack slide-in-right">
-
-                    <div class="HStack">
-                        <Popover 
-                            trigger={
-                                <div class='text-as-button ZStack' style="margin-right: calc(var(--size-sm0)* -1);" >
-                                    <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM18 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" fill="oklch(var(--gray-95))"/></svg>
-                                </div>
-                            }
-                        >
-                            <menu class="glass subtle-shadow menu-horizontal">
-                                <li>
-                                    <DeleteMenu  content= {
-                                        <button  data-action='delete' id={entry.id?.toString() ?? ''}>Delete</button>  
-                                    }></DeleteMenu>                                
-                                </li>
-                            </menu>
-                        </Popover>                  
-                    </div>
-
-                    <div data-swapy-handle style="width: var(--size-md2);display: flex;justify-content: flex-end;" > 
-                        <div class="swapy-handle"></div> 
-                    </div>
-
-                </div> }
-            </div>
-        </li> 
+            </li> 
+        )))}
     </>);
 }
+
+export default EmailsList
--- src/components/ui/Drawer.tsx
+++ src/components/ui/Drawer.tsx
@@ -48,14 +48,13 @@ const DrawerComponent: ParentComponent<DrawerComponentProps> = (props) => {
               {props.content}
             </div>
             
-            <div class={`top gradient-blur ${styles.header}`}>
-              <div style="grid-column: 2; ">{props.title}</div>
-              <button
-                style="justify-self: end; align-self: self-start; padding-top: var(--size-sm0);"
-                class={`not-prose ${styles.closeButton}`}
-                popovertarget={props.id}
-                popovertargetaction="hide"
-              >
+            <div class={`top gradient-blur ${styles.header}`}> </div>
+            <div style="isolation: isolate;" class="top">{props.title}</div>
+              <button style="padding-top: var(--size-sm0);"
+              class="not-prose top-trailing"
+              popovertarget={props.id}
+              popovertargetaction="hide">
+
                 <svg style="opacity:20%;
                     width: auto;
                     height: var(--size-md1);
@@ -63,7 +62,7 @@ const DrawerComponent: ParentComponent<DrawerComponentProps> = (props) => {
                      <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2Zm3.53 6.47-.084-.073a.75.75 0 0 0-.882-.007l-.094.08L12 10.939l-2.47-2.47-.084-.072a.75.75 0 0 0-.882-.007l-.094.08-.073.084a.75.75 0 0 0-.007.882l.08.094L10.939 12l-2.47 2.47-.072.084a.75.75 0 0 0-.007.882l.08.094.084.073a.75.75 0 0 0 .882.007l.094-.08L12 13.061l2.47 2.47.084.072a.75.75 0 0 0 .882.007l.094-.08.073-.084a.75.75 0 0 0 .007-.882l-.08-.094L13.061 12l2.47-2.47.072-.084a.75.75 0 0 0 .007-.882l-.08-.094-.084-.073.084.073Z" fill="oklch(var(--gray-95))"/>
                   </svg>
               </button>
-            </div>
+            
 
 
           </div>
