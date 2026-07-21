3927c0d537e8672744df139f7cd57f4a42a0b41d
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2026-07-21 08:31:07 +0330

    Update Emails.tsx

--- src/components/Emails.tsx
+++ src/components/Emails.tsx
@@ -33,23 +33,6 @@ const EmailsList = (entries: ReadAllResultTypes["Emails"], isEditing: boolean) =
                             </button>
                         </div>
                         <div class="HStack slide-in-right">
-                            <div class="HStack">
-                                <Popover
-                                    trigger={
-                                        <div class='text-as-button ZStack' style="margin-right: calc(var(--size-sm0)* -1);">
-                                            <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM18 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" fill="oklch(var(--gray-95))"/></svg>
-                                        </div>
-                                    }
-                                >
-                                    <menu class="glass subtle-shadow menu-horizontal">
-                                        <li>
-                                            <DeleteMenu content={
-                                                <button onClick={() => entry.id && email.deleteEmail(entry.id)}>Delete</button>
-                                            }></DeleteMenu>
-                                        </li>
-                                    </menu>
-                                </Popover>
-                            </div>
                             <div data-swapy-handle style="width: var(--size-md2);display: flex;justify-content: flex-end;">
                                 <div class="swapy-handle"></div>
                             </div>
