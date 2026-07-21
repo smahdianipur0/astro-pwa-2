064792d4ad13995dabced4a8b6e4dcb91af3c888
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2026-07-21 09:01:46 +0330

    Update SwiperComponent.tsx

--- src/components/ui/SwiperComponent.tsx
+++ src/components/ui/SwiperComponent.tsx
@@ -10,17 +10,17 @@ export interface SwiperComponentProps {
 const SwiperComponent: ParentComponent<SwiperComponentProps> = (props) => {
   return (
     <div class={styles.row}>
-      <div class={`HStack ${styles.swipe}`} data-swipe>
+      <div class={styles.swipe} data-swipe>
         <div class={`HStack ${styles.main}`}>
           <div class="x-stretch">{props.children}</div>
-          
+
           <details class={`HStack ${styles.details}`} style="font-size: var(--size-sm0);" data-details {...rowActionsGroupName}>
             <summary class={`trailing ${styles.summary}`} aria-label="Row actions">
               ⋮
             </summary>
             <div class={`HStack x-stretch y-stretch ${styles.optionsRow}`}>{props.options}</div>
           </details>
-          
+
         </div>
         <div class={styles.reveal} />
       </div>
