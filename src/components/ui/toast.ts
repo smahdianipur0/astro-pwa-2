8fbe7f934a9239e25f9905babe824d81c7d261ec
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2026-07-24 19:37:12 +0330

    Update toast.ts

--- src/components/ui/toast.ts
+++ src/components/ui/toast.ts
@@ -5,7 +5,7 @@ export function showToast(Clipboard? : string) {
   const clipboardElement = document.getElementById('clipboard')!;
   if (toastElement) {
 
-    toastElement.style.marginTop = 'var(--size-lg1)';
+    toastElement.style.marginTop = 'var(--size-sm4)';
     toastElement.style.scale = '1';
     toastElement.style.transition = 'margin-top 0.25s var(--move), scale 0.5s var(--appear)';
     if(Clipboard && clipboardElement){
@@ -16,6 +16,9 @@ export function showToast(Clipboard? : string) {
       toastElement.style.marginTop = '-100%';
       toastElement.style.scale = '0.55';
       toastElement.style.transition = 'margin-top 0.4s ease-in, scale 0.4s ease-in';
+      if(Clipboard && clipboardElement){
+        clipboardElement.textContent = ``
+      }
     }, 2000);
   }
 }
