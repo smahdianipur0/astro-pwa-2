1858064cb0dfa6f86293923f9392b2c1788a8864
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2026-07-26 07:55:39 +0330

    Update toast.ts

--- src/components/ui/toast.ts
+++ src/components/ui/toast.ts
@@ -1,24 +1,25 @@
 let timeoutId: number | undefined;
 
-export function showToast(Clipboard? : string) {
-  const toastElement = document.getElementById('toast')!;
-  const clipboardElement = document.getElementById('clipboard')!;
-  if (toastElement) {
+export function showToast(clipboard?: string) {
+  const toastElement = document.getElementById('toast');
+  const clipboardElement = document.getElementById('clipboard');
 
-    toastElement.style.marginTop = 'var(--size-sm4)';
-    toastElement.style.scale = '1';
-    toastElement.style.transition = 'margin-top 0.25s var(--move), scale 0.5s var(--appear)';
-    if(Clipboard && clipboardElement){
-      clipboardElement.textContent = `Clipboard: ${Clipboard}`
-    }
+  if (!toastElement || !clipboardElement) return;
 
-    timeoutId = window.setTimeout(() => {
-      toastElement.style.marginTop = '-100%';
-      toastElement.style.scale = '0.55';
-      toastElement.style.transition = 'margin-top 0.4s ease-in, scale 0.4s ease-in';
-      if(Clipboard && clipboardElement){
-        clipboardElement.textContent = ``
-      }
-    }, 2000);
+  clipboardElement.textContent = '';
+  if (timeoutId) { clearTimeout(timeoutId);}
+
+  if (clipboard) {
+    clipboardElement.textContent = `Clipboard: ${clipboard}`;
   }
+
+  toastElement.style.marginTop = 'var(--size-sm3)';
+  toastElement.style.scale = '1';
+  toastElement.style.transition = 'margin-top 0.25s var(--move), scale 0.5s var(--appear)';
+
+  timeoutId = window.setTimeout(() => {
+    toastElement.style.marginTop = '-100%';
+    toastElement.style.scale = '0.55';
+    toastElement.style.transition = 'margin-top 0.4s ease-in, scale 0.4s ease-in';
+  }, 2000);
 }
