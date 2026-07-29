8570bc618a8ece98435209f777f2a1871f287cd7
smahdianipur0 <157228225+smahdianipur0@users.noreply.github.com>
2026-07-29 06:32:01 +0430

    removed deleteMenu, used swiper for pass

--- src/components/RecentDelPass.tsx
+++ src/components/RecentDelPass.tsx
@@ -1,6 +1,7 @@
 import type { ReadResultTypes, ReadAllResultTypes } from "../utils/surrealdb-indexed";
 import Popover from "./ui/Popover";
-import DeleteMenu from './ui/DeleteMenu'
+import Swiper from "./ui/Swiper.tsx";
+import TwoStepAction from "./ui/TwoStepAction.tsx";
 import { showToast } from "../components/ui/toast.ts";
 import { tempList } from "../logic/tempList";
 
@@ -23,54 +24,44 @@ const RecentDelPassItems = (entries: ReadAllResultTypes["RecentDelPass"] | [])=>
     
       entries.map((entry) => (
         <li style="background-color: transparent;">
-          <div>
-
-            <small class="ellipsis"style="width: var(--size-xl2)">
-              {entry.title || 'untitled'}
-            </small>
-
-            <button  class="not-prose ellipsis"
-            style="text-align: start; width: var(--size-xl2);"
-            onClick={() => {
-                navigator.clipboard.writeText(entry.password ?? "");
-                showToast();
-              }}
-            > {entry.password ?? ''}</button>
-
-          </div>
-
-          <div class="ZStack" style="justify-content: flex-end;">
-
-            <Popover 
-              trigger={
-                  <div class='text-as-button ZStack' style="margin-right: calc(var(--size-sm0)* -1);" >
-                    <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM18 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" fill="oklch(var(--gray-95))"/></svg>
-                  </div>
-              }
-            >
-              <menu class="glass subtle-shadow menu-horizontal" >
+          <Swiper
+            options={
+              <>
                 <li>
-                  <DeleteMenu  content= {
-                    <button onClick={() => entry.id && tempList.deleteRecentDelEntris(entry.id)}>Delete</button>  
-                  }></DeleteMenu>                          
+                  <TwoStepAction color="var(--danger)" icon="var(--icon-delete)">
+                    <button onClick={() => entry.id && tempList.deleteRecentDelEntris(entry.id)}>Delete</button> 
+                  </TwoStepAction>                         
                 </li>
 
                 <li>
-                  <button  class="not-prose HStack" 
-                    style="color: oklch(var(--gray-95)); gap: var(--size-xs2);"
+                  <button  class="not-prose HStack" style="color: oklch(var(--gray-95)); gap: var(--size-xs2);"
                     onClick={() => openRestoreDialog(entry)}> 
-                    
+           
                     <svg style="width: var(--size-sm3); height: var(--size-sm3); padding-top: 2px;" viewBox="0 0 24 24" fill="none" >
                       <path d="M4.75 2a.75.75 0 0 1 .743.648l.007.102v5.69l4.574-4.56a6.41 6.41 0 0 1 8.879-.179l.186.18a6.41 6.41 0 0 1 0 9.063l-8.846 8.84a.75.75 0 0 1-1.06-1.062l8.845-8.838a4.91 4.91 0 0 0-6.766-7.112l-.178.17L6.562 9.5h5.688a.75.75 0 0 1 .743.648l.007.102a.75.75 0 0 1-.648.743L12.25 11h-7.5a.75.75 0 0 1-.743-.648L4 10.25v-7.5A.75.75 0 0 1 4.75 2Z" />
-                    </svg>
+                    </svg>   
 
-                    <div>Rstrore</div>
-                    
                   </button>
                 </li>
-              </menu>
-            </Popover> 
-          </div>
+              </>
+            }
+          >
+
+            <div>
+
+              <small class="ellipsis"style="width: var(--size-xl2)">
+                {entry.title || 'untitled'}
+              </small>
+
+              <button  class="not-prose ellipsis" style="text-align: start; width: var(--size-xl2);"
+              onClick={() => {
+                  navigator.clipboard.writeText(entry.password ?? "");
+                  showToast();
+                }}
+              > {entry.password ?? ''}</button>
+
+            </div>
+          </Swiper>
         </li>
       ))
     )}
--- src/components/TempList.tsx
+++ src/components/TempList.tsx
@@ -1,5 +1,5 @@
-import Popover from "./ui/Popover";
-import DeleteMenu from "./ui/DeleteMenu";
+import Swiper from "./ui/Swiper.tsx";
+import TwoStepAction from "./ui/TwoStepAction.tsx";
 import { tempList } from "../logic/tempList";
 import { showToast } from "../components/ui/toast.ts";
 import type { ReadResultTypes, ReadAllResultTypes } from "../utils/surrealdb-indexed";
@@ -22,10 +22,30 @@ const TempListItems = (entries: ReadAllResultTypes["PasswordEntry"] | [])=> {
       <small style="text-align: center; padding-block: var(--size-sm3)"> No records found </small>
       ):(
     
-      entries.map((entry) => (        
+      entries.map((entry) => (
         <li style="background-color:transparent;">
+        <Swiper options={
+          <>
+            <li>
+              <TwoStepAction color="var(--danger)" icon="var(--icon-delete)">
+                <button onClick={() => entry.id && tempList.deleteEntries(entry.id)}> Delete</button>
+              </TwoStepAction>
+            </li>
+            <li>
+                <button
+                  class="not-prose HStack"
+                  style="color: oklch(var(--gray-95)); gap: var(--size-xs2);"
+                  onClick={() => openEditDialog(entry)}
+                >
+                  <svg style="width: var(--size-sm3); height: var(--size-sm3);" viewBox="0 0 24 24" fill="none">
+                    <path d="M21.03 2.97a3.578 3.578 0 0 1 0 5.06L9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L15.97 2.97a3.578 3.578 0 0 1 5.06 0ZM15 6.06 5.062 16a.75.75 0 0 0-.193.333l-1.05 3.85 3.85-1.05A.75.75 0 0 0 8 18.938L17.94 9 15 6.06Zm2.03-2.03-.97.97L19 7.94l.97-.97a2.079 2.079 0 0 0-2.94-2.94Z" />
+                  </svg> 
+                </button>
+              </li>
+          </>
+        }>
+          
           <div class="VStack leading" style="--gap: 0;">
-
             <small class="ellipsis" style="width: var(--size-xl2)"> {entry.title || "untitled"} </small>
 
             <button class="not-prose ellipsis" style="text-align: start; width: var(--size-xl2); line-height: var(--size-sm4);  "
@@ -36,40 +56,8 @@ const TempListItems = (entries: ReadAllResultTypes["PasswordEntry"] | [])=> {
             >{entry.password ?? ""}</button>
 
           </div>
-
-          <div>
-            <Popover
-              trigger={
-                <div class="text-as-button ZStack" style="margin-right: calc(var(--size-sm0) * -1);">
-                  <svg width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM14 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM18 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" fill="oklch(var(--gray-95))"/>
-                  </svg>
-                </div>
-              }
-            >
-              <menu id={`menu-${entry.id?.toString()}`} class="glass subtle-shadow menu-horizontal">
-                <li>
-                  <DeleteMenu
-                    content={
-                      <button onClick={() => entry.id && tempList.deleteEntries(entry.id)}> Delete</button>
-                    }
-                  />
-                </li>
-                <li>
-                  <button
-                    class="not-prose HStack"
-                    style="color: oklch(var(--gray-95)); gap: var(--size-xs2);"
-                    onClick={() => openEditDialog(entry)}
-                  >
-                    <svg style="width: var(--size-sm3); height: var(--size-sm3);" viewBox="0 0 24 24" fill="none">
-                      <path d="M21.03 2.97a3.578 3.578 0 0 1 0 5.06L9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L15.97 2.97a3.578 3.578 0 0 1 5.06 0ZM15 6.06 5.062 16a.75.75 0 0 0-.193.333l-1.05 3.85 3.85-1.05A.75.75 0 0 0 8 18.938L17.94 9 15 6.06Zm2.03-2.03-.97.97L19 7.94l.97-.97a2.079 2.079 0 0 0-2.94-2.94Z" />
-                    </svg>
-                    <div>Update</div>
-                  </button>
-                </li>
-              </menu>
-            </Popover>
-          </div>
+        
+        </Swiper>
         </li>
       ))
     )}
